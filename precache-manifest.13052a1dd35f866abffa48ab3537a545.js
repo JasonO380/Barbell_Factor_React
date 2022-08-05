18490d71e3d2a83384ce868c795b6fa345017cd5
Jason Ollada <JMOllada@gmail.com>
2022-08-05 18:16:50 -0500

    Revert "Updates"
    This reverts commit eb3fcaeeeb08c4232666cf012d71c7914819b6c8.

---
+++ precache-manifest.13052a1dd35f866abffa48ab3537a545.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "10e9e861d3bf9da837c8",
+    "url": "/Barbell_Factor_React/static/css/main.9cefe908.chunk.css"
+  },
+  {
+    "revision": "10e9e861d3bf9da837c8",
+    "url": "/Barbell_Factor_React/static/js/main.10e9e861.chunk.js"
+  },
+  {
+    "revision": "e4c123256ec82141fcb1",
+    "url": "/Barbell_Factor_React/static/js/1.e4c12325.chunk.js"
+  },
+  {
+    "revision": "45b5dd506d072975f67b",
+    "url": "/Barbell_Factor_React/static/css/2.f3ddd8c7.chunk.css"
+  },
+  {
+    "revision": "45b5dd506d072975f67b",
+    "url": "/Barbell_Factor_React/static/js/2.45b5dd50.chunk.js"
+  },
+  {
+    "revision": "122a9f33d94dbf48a61c",
+    "url": "/Barbell_Factor_React/static/js/runtime~main.122a9f33.js"
+  },
+  {
+    "revision": "ec8a2badf189b6f51d35797aa8ed79db",
+    "url": "/Barbell_Factor_React/static/media/barbellfactor.ec8a2bad.jpeg"
+  },
+  {
+    "revision": "c2e0092361af3c5c7b92d52c437d1303",
+    "url": "/Barbell_Factor_React/static/media/profilepic.c2e00923.jpeg"
+  },
+  {
+    "revision": "3ccfdf1a267946876c88a4d17af69b88",
+    "url": "/Barbell_Factor_React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Barbell_Factor_React/precache-manifest.99c2a62480415c80b4d0355ab833b145.js"
+  "/Barbell_Factor_React/precache-manifest.13052a1dd35f866abffa48ab3537a545.js"
 );
 
 workbox.clientsClaim();
